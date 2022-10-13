e5c8f2dc0f9071c56dc7f48e33e602245ab5258b
unknown <johnlaw114@gmail.com>
2022-10-13 20:46:35 +0900

    added working button fetch api

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-//fetch api later we can implement 
-//more than one page and based on titles params
-async function getMovieTitles() {
-    const apiUrl = "https://jsonmock.hackerrank.com/api/movies/search/?Title=";
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data); 
-}
+window.addEventListener("DOMContentLoaded", () => {
+  
+    document.getElementById("search__submitbtn").addEventListener("click", getMovieTitles, false);
+    const input = document.getElementById("searchbox");
 
-getMovieTitles();
+  //enter now with the function will behave the same ways as our button click
+  input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("search__submitbtn").click();
+    }
+  });
+
+  //fetch api later we can implement
+  //more than one page and based on titles params
+  async function getMovieTitles() {
+    const apiUrl = "https://jsonmock.hackerrank.com/api/movies/search/?Title=";
+    const data = await fetch(apiUrl).then((res) => res.json());
+    console.log(data);
+  }
+
+});
